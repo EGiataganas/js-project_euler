04844a1cf1a1f08f51ca8d55de9b7130414b96df
Evangelos Giataganas <e.giataganas@gmail.com>
2015-02-17 18:08:53 +0000

    add some more things to communicate with index page

--- script/problem1.js
+++ script/problem1.js
@@ -7,3 +7,11 @@ var sumOfMultiplesOf3and5 = function (input){
 	};
 	return sum;
 }
+
+var numberInput1 = document.querySelector("[name=inputNumber1]");
+var resultArea1 = document.querySelector(".sum-of-multiples-of-3-and-5");
+
+numberInput1.addEventListener('input', function() {
+	var val = sumOfMultiplesOf3and5(this.value);
+	resultArea1.textContent = val;
+});
